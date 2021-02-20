7166426070da05052010f2ea1c8d5495d39623b7
yaocs2 <yaocs2@midea.com>
2021-02-21 00:36:37 +0800

    foodie-cloud> foodie-cloud-web-components> web组件工程初始化

---
+++ common/foodie-cloud-web-components/src/main/java/com/imooc/config/WebMvcConfig.java
@@ -0,0 +1,39 @@
+package com.imooc.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebMvcConfig implements WebMvcConfigurer {
+
+    // FIXME 文件上传配置类: 未配置
+//    @Autowired
+//    private FileUpload fileUpload;
+
+    // 实现静态资源的映射
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/**")
+                .addResourceLocations("classpath:/META-INF/resources/")  // 映射swagger2 -> 保持原有的资源映射
+                .addResourceLocations("file:/workspaces/images/");  // 映射本地静态资源 -> 相当于用域名替换调这些路径, 就是写死, 因为要存到数据库中的, 哪个环境都一样
+    }
+
+    // FIXME SpringAutoConfigure依赖未引用
+//    @Bean
+//    public RestTemplate restTemplate(RestTemplateBuilder builder) {
+//        return builder.build();
+//    }
+
+    // FIXME 自定义Token拦截器未配置
+//    @Bean
+//    public UserTokenInterceptor userTokenInterceptor() {
+//        return new UserTokenInterceptor();
+//    }
+    // FIXME 自定义Token拦截器未配置
+//    @Override
+//    public void addInterceptors(InterceptorRegistry registry) {
+//        registry.addInterceptor(userTokenInterceptor()).addPathPatterns("/hello/hello");
+//         TODO 继续添加待拦截的路由
+//    }
+}
---
+++ common/foodie-cloud-web-components/src/main/java/com/imooc/controller/BaseController.java
@@ -0,0 +1,70 @@
+package com.imooc.controller;
+
+import org.springframework.stereotype.Controller;
+
+import java.io.File;
+
+@Controller
+public class BaseController {
+
+    public static final String FOODIE_SHOPCART = "shopcart";
+
+    public static final Integer COMMON_PAGE_SIZE = 10;
+    public static final Integer PAGE_SIZE = 20;
+
+    public static final String REDIS_USER_TOKEN = "redis_user_token";
+
+    // 支付中心的调用地址
+    String paymentUrl = "http://payment.t.mukewang.com/foodie-payment/payment/createMerchantOrder";		// produce
+
+    // 微信支付成功 -> 支付中心 -> 天天吃货平台
+    //                       |-> 回调通知的url
+//    String payReturnUrl = "http://api.z.mukewang.com/foodie-dev-api/orders/notifyMerchantOrderPaid";
+    // 本地地址
+//    String payReturnUrl = "http://38vf4v.natappfree.cc/orders/notifyMerchantOrderPaid";
+    // 虚拟机地址
+    String payReturnUrl = "http://b85hwu.natappfree.cc/foodie-dev-api/orders/notifyMerchantOrderPaid";
+
+    // 用户上传头像的位置
+    public static final String IMAGE_USER_FACE_LOCATION = File.separator + "workspaces" +
+                                                            File.separator + "images" +
+                                                            File.separator + "foodie" +
+                                                            File.separator + "faces";
+//    public static final String IMAGE_USER_FACE_LOCATION = "/workspaces/images/foodie/faces";
+
+    // FIXME 订单域服务未抽取
+//    @Autowired
+//    public MyOrdersService myOrdersService;
+
+    // FIXME Redis未引用
+//    @Autowired
+//    private RedisOperator redisOperator;
+
+    // FIXME 订单域服务未抽取
+//    /**
+//     * 用于验证用户和订单是否有关联关系，避免非法用户调用
+//     * @return
+//     */
+//    public IMOOCJSONResult checkUserOrder(String userId, String orderId) {
+//        Orders order = myOrdersService.queryMyOrder(userId, orderId);
+//        if (order == null) {
+//            return IMOOCJSONResult.errorMsg("订单不存在！");
+//        }
+//        return IMOOCJSONResult.ok(order);
+//    }
+//
+    // FIXME 用户域服务未抽取
+//    // 改用UsersVO => 忽略隐私信息, 生成用户token, 存入redis会话
+//    public UsersVO conventUsersVO(Users userResult) {
+//        // 生成用户token, 存入redis会话
+//        String uniqueToken = UUID.randomUUID().toString().trim();
+//        redisOperator.set(REDIS_USER_TOKEN + ":" + userResult.getId(), uniqueToken);
+//
+//        // 复制Users属性
+//        UsersVO usersVO = new UsersVO();
+//        BeanUtils.copyProperties(userResult, usersVO);
+//        usersVO.setUserUniqueToken(uniqueToken);
+//
+//        return usersVO;
+//    }
+}
