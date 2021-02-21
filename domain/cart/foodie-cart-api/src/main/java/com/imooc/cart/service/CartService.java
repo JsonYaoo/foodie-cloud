258b0ed07c75a6dfe79d2848eb8a2aa97a391665
yaocs2 <yaocs2@midea.com>
2021-02-21 16:32:19 +0800

    foodie-cloud> foodie-cart-api> cart域api服务接口工程初始化

---
+++ domain/cart/foodie-cart-api/src/main/java/com/imooc/cart/service/CartService.java
@@ -0,0 +1,26 @@
+package com.imooc.cart.service;
+
+import com.imooc.bo.ShopcartBO;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+/**
+ * 购物车域: API服务接口抽取
+ */
+@RequestMapping("cart-api")
+public interface CartService {
+
+    @PostMapping("addItem")
+    public boolean addItemToCart(@RequestParam("userId") String userId,
+                                 @RequestBody ShopcartBO shopcartBO);
+
+    @PostMapping("removeItem")
+    public boolean removeItemFromCart(@RequestParam("userId") String userId,
+                                      @RequestParam("itemSpecId") String itemSpecId);
+
+    @PostMapping("clearCart")
+    public boolean clearCart(@RequestParam("userId") String userId);
+
+}
