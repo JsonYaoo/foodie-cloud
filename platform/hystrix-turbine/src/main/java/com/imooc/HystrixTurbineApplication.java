58b008e8786a333c2ef2fd2ae79a1c89a680e6a2
yaocs2 <yaocs2@midea.com>
2021-02-27 12:25:17 +0800

    foodie-cloud> 集成Turbine组件应用

---
+++ platform/hystrix-turbine/src/main/java/com/imooc/HystrixTurbineApplication.java
@@ -0,0 +1,22 @@
+package com.imooc;
+
+import org.springframework.boot.WebApplicationType;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.turbine.EnableTurbine;
+
+/**
+ * Turbine组件应用
+ */
+@EnableDiscoveryClient
+@EnableTurbine
+@EnableAutoConfiguration
+public class HystrixTurbineApplication {
+
+    public static void main(String[] args) {
+        new SpringApplicationBuilder(HystrixTurbineApplication.class)
+                .web(WebApplicationType.SERVLET)
+                .run(args);
+    }
+}
